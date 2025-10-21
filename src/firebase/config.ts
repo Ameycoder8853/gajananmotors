0933b13f312d7c8788588a84d7ec492fadb03c25
Amey Patil <amey35195@gmail.com>
2025-10-21 09:07:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3421303385-44fc1",
+  "appId": "1:639177828437:web:fd3c00288699576d9d8cdc",
+  "apiKey": "AIzaSyB1LT_NIyg_RIFaJvH46HY-HWRS68-4H6o",
+  "authDomain": "studio-3421303385-44fc1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "639177828437"
+};
