a68ff3f2a9542720f5c298e9ee8749ea86b190a7
Amey Patil <amey35195@gmail.com>
2025-10-28 02:49:36 +0000

    take this update in env file "// Import the functions you need from the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-3421303385-44fc1",
-  "appId": "1:639177828437:web:fd3c00288699576d9d8cdc",
-  "apiKey": "AIzaSyB1LT_NIyg_RIFaJvH46HY-HWRS68-4H6o",
-  "authDomain": "studio-3421303385-44fc1.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "639177828437",
-  "storageBucket": "videomatic-ai-5c2b2.appspot.com"
+  apiKey: "AIzaSyD_7EAIeJ7PLiABKk0om-dISJxvCGMjhzM",
+  authDomain: "gajananmotors-2033e.firebaseapp.com",
+  projectId: "gajananmotors-2033e",
+  storageBucket: "gajananmotors-2033e.appspot.com",
+  messagingSenderId: "465027127739",
+  appId: "1:465027127739:web:6e0f1350d4cbc3fe594c9e",
+  measurementId: "G-6RTZ9RGMQK"
 };
